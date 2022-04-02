523bf93c279bf1a3d6196a96df4500f845a6fdf7
/Creza <98697142+CrezaD@users.noreply.github.com>
2022-04-03 02:41:06 +0300

    Create argüman.c

---
+++ argüman.c
@@ -0,0 +1,18 @@
+namespace CsharpFundamentals 
+{
+    class Program
+    {
+        static voic Main(string[] args)
+        {
+            int Creza=Developer
+            int Raxe=Creza
+
+            int hepsi;
+
+            hepsi = Creza = Raxe
+            Console.WriteLine(hepsi)
+
+            Console.ReadLine();
+        }
+    }
+}
